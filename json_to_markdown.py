a1cd39d43dba3305f0efb40d7fac541cdbb1315a
Ben Johnson <johnsonb@ucar.edu>
2024-01-24 13:46:38 -0700

    Update json_to_markdown to include book chapters and include a sample json file

--- json_to_markdown.py
+++ json_to_markdown.py
@@ -3,7 +3,7 @@
 import json
 from collections import OrderedDict
 
-f = open('dart-web.json')
+f = open('dart-publications.json')
 
 publications = json.load(f)
 
@@ -21,20 +21,19 @@ aliases     :
    - /pages/Publications.html
 ---
 
-This list gets updated as frequently as possible, however some more
-recent publications may also be available on the UCAR/NCAR online
-database [Opensky](https://opensky.ucar.edu/).
-Simply use it and search for "ensemble data assimilation" (for
-example). Many, if not most, are related to DART. The following list
-also contains some publications from our collaborators. If you would
-like to list your publication that uses DART, please let us know!
-(dart@ucar.edu)
+The following publication list contains known publications that use DART.
 
-Please use the folowing to cite DART:
+Please contact [dart@ucar.edu](mailto:dart@ucar.edu) to add your publication
+to the list.
 
-> The Data Assimilation Research Testbed (Version X.Y.Z) [Software]. (2019). Boulder, Colorado: UCAR/NCAR/CISL/DAReS. [http://doi.org/10.5065/D6WQ0202](http://doi.org/10.5065/D6WQ0202)  
+Recent publications coauthored by NCAR staff should be freely available in the
+NSF NCAR online database known as [Opensky](https://opensky.ucar.edu/) by
+searching for a given publication's title.
 
-Update the DART version and year as appropriate.
+To cite DART in your publication, please use this citation updating the DART
+version and year as appropriate:
+
+> The Data Assimilation Research Testbed (Version X.Y.Z) [Software]. (2024). Boulder, Colorado: NSF NCAR/CISL/DAReS. [http://doi.org/10.5065/D6WQ0202](http://doi.org/10.5065/D6WQ0202)  
 
 The seminal reference is:   
 
@@ -48,32 +47,29 @@ The Data Assimilation Research Testbed: A Community Facility.
 
 year_header = """
 ---
-{current_year}
+## {current_year}
 ---
 """
 
-publication_string = """
+article_string = """
 ![](/images/pin4.gif) **{authors}**, {publication_year}  
     {title}.  
-    *{journal}*,{volume}{page}, [{doi}]({url}).
+    *{journal}*,{volume}{page}[{doi}]({url})
 """
+
+chapter_string = """
+![](/images/pin4.gif) **{authors}**, {publication_year}  
+    {title},  
+    {book}.  
+    {editors}, {publisher}, ISBN: {isbn}
+"""
+
+
 
 for publication in publications:
 
     publication_year = publication['issued']['date-parts'][0][0]
     title = publication['title']
-    journal = publication['container-title']
-    try:
-        volume = ' **' + publication['volume']+'**, '
-    except:
-        volume = ' '
-    try:
-        page = publication['page']+', '
-    except:
-        page = ' '
-    
-    doi = 'doi.org/' + publication['DOI']
-    url = 'https://' + doi
 
     if current_year != publication_year and current_year == '2005':
         current_year = before_dart_string
@@ -91,11 +87,60 @@ for publication in publications:
             initials += name[0] + '.'
 
         if iauthor == 0:
-            authors = initials + ' ' + this_author['family']
+            authors = this_author['family'] + ' ' + initials
+        elif iauthor + 1 == len(publication['author']):
+            authors += ' & ' + initials + ' ' + this_author['family']
         else:
-            authors += ', ' + this_author['family'] + ' ' + initials
-    
-    page_string += publication_string.format(authors=authors, publication_year=publication_year, title=title, journal=journal, volume=volume, page=page, doi=doi, url=url)
-    
+            authors += ', ' + initials + ' ' + this_author['family']
+
+    if publication['type'] == 'article-journal':
+
+        journal = publication['container-title']
+        
+        try:
+            volume = ' **' + publication['volume']+'**, '
+        except:
+            volume = ' '
+        try:
+            page = publication['page']+', '
+        except:
+            page = ' '
+        
+        doi = 'doi:' + publication['DOI']
+        url = 'https://doi.org/' + doi
+
+        page_string += article_string.format(authors=authors, publication_year=publication_year, title=title, journal=journal, volume=volume, page=page, doi=doi, url=url)
+
+    elif publication['type'] == 'chapter':
+
+        book = publication['container-title']
+        publisher = publication['publisher']
+        isbn = publication['ISBN']
+
+        for ieditor, this_editor in enumerate(publication['editor']):
+            initials = ''
+            first_middle = this_editor['given'].split(' ')
+            for name in first_middle:
+                initials += name[0] + '.'
+            if ieditor == 0:
+                editors = this_editor['family'] + ' ' + initials
+            elif ieditor + 1 == len(publication['editor']):
+                editors += ' & ' + initials + ' ' + this_editor['family']
+            else:
+                editors += ', ' + initials + ' ' + this_editor['family']
+        
+        page_string += chapter_string.format(authors=authors, publication_year=publication_year, title=title, book=book, editors=editors, publisher=publisher, isbn=isbn)
+
+page_string += """
+
+----
+
+"""
+
 print(page_string)
-    
+
+output_file = '../dart-web/content/publications/_index.md'
+
+f = open(output_file, 'w')
+f.write(page_string)
+f.close()
