3534b9914e03e17df40d6686aafd1fed5419b35a
Ben Johnson <johnsonb@ucar.edu>
2024-08-26 13:49:20 -0600

    Update dissertations-and-theses with better error messages.

--- dissertations-and-theses.py
+++ dissertations-and-theses.py
@@ -42,8 +42,14 @@ dissertation_or_thesis_string = """
 
 for publication in publications:
 
+    author = publication_year = title = genre = publisher = publisher_place = url = ''
+
     publication_year = publication['issued']['date-parts'][0][0]
-    title = publication['title']
+    
+    try:
+        title = publication['title']
+    except:
+        print("A recently added Thesis or Dissertation is missing a title. Update the document's entry, export it, and run this script again.")
 
     if current_year != publication_year and current_year == '2005':
         current_year = before_dart_string
@@ -53,26 +59,43 @@ for publication in publications:
         current_year = publication_year
         page_string += year_header.format(current_year = current_year)
 
-    for iauthor, this_author in enumerate(publication['author']):
+    try:
+        for iauthor, this_author in enumerate(publication['author']):
 
-        initials = ''
-        first_middle = this_author['given'].split(' ')
-        for name in first_middle:
-            initials += name[0] + '.'
+            initials = ''
+            first_middle = this_author['given'].split(' ')
+            for name in first_middle:
+                initials += name[0] + '.'
 
-        author = this_author['family'] + ' ' + initials
+            author = this_author['family'] + ' ' + initials
+    except:
+        print(title + " is missing an author. Update the document's entry, export it, and run this script again.")        
 
-    if publication['genre'] == 'Thesis':
-        genre = 'Master\'s thesis'
-    elif publication['genre'] == 'Dissertation':
-        genre = 'Doctoral dissertation'
+    try:
+        if publication['genre'] == 'Thesis':
+            genre = 'Master\'s thesis'
+        elif publication['genre'] == 'Dissertation':
+            genre = 'Doctoral dissertation'
+    except:
+        print(title + " is missing a Thesis or Dissertation designation. Update the document's entry, export it, and run this script again.")        
 
     if title[-1] != '.' and title[-1] != '?':
         title += '.'
 
-    url = publication['URL']
-    publisher = publication['publisher']
-    publisher_place = publication['publisher-place']
+    try:
+        url = publication['URL']
+    except:
+        print(title + " is missing a URL. Update the document's entry, export it, and run this script again.")        
+
+    try:
+        publisher = publication['publisher']
+    except:
+        print(title + " is missing a university. Update the document's entry, export it, and run this script again.")
+    
+    try:
+        publisher_place = publication['publisher-place']
+    except:
+        print(title + " is missing the city in which the university is located. Update the document's entry, export it, and run this script again.")
     
     page_string += dissertation_or_thesis_string.format(author=author, publication_year=publication_year, title=title, genre=genre, publisher=publisher, publisher_place=publisher_place, url=url)
 
@@ -82,7 +105,7 @@ page_string += """
 
 """
 
-print(page_string)
+# print(page_string)
 
 output_file = '../dart-web/content/dissertations-and-theses/_index.md'
 
